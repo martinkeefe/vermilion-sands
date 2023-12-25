cb480fcff8f1c0e26844a985297d0a85ba1dce0f
martinkeefe <martin.keefe@outlook.com>
2023-12-25 17:56:34 +0000

    Tweaked for Vercel.

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
-import vike from "vike/plugin";
+import { defineConfig } from "vite";
+import ssr from "vike/plugin";
+import vercel from "vite-plugin-vercel";
 
-export default {
-  plugins: [vike()],
-};
+export default defineConfig(async ({ command, mode }) => {
+  return {
+    plugins: [ssr(), vercel()],
+  };
+});
